5f37009a68310faae6b3820a3afe7dfc1b4d25e9
willtwr <wrtan.edu@gmail.com>
2025-01-16 10:58:24 +0800

    remove context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Home from 'pages/Home';
-import Theme from 'theme';
+import theme from 'theme';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
-  const theme = Theme();
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
--- src/theme.js
+++ src/theme.js
@@ -1,38 +1,30 @@
-import { createContext } from "react";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { deepPurple, teal } from '@mui/material/colors';
 
-export const ThemeContext = createContext({
-    darkMode: false,
+const theme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            light: deepPurple[200],
+            main: deepPurple[400],
+            dark: deepPurple[800]
+        },
+        secondary: {
+            light: teal[100],
+            main: teal[300],
+            dark: teal[800]
+        },
+        background: {
+            default: deepPurple[900]
+        },
+    },
+    components: {
+        MuiPaper: {
+            defaultProps: {
+                elevation: 5
+            }
+        },
+    },
 });
 
-function Theme() {
-    const theme = createTheme({
-        palette: {
-            mode: "light",
-            primary: {
-                light: deepPurple[200],
-                main: deepPurple[400],
-                dark: deepPurple[800]
-            },
-            secondary: {
-                light: teal[100],
-                main: teal[300],
-                dark: teal[800]
-            },
-            background: {
-                default: deepPurple[900]
-            },
-        },
-        components: {
-            MuiPaper: {
-                defaultProps: {
-                    elevation: 5
-                }
-            },
-        },
-    });
-    return responsiveFontSizes(theme);
-};
-
-export default Theme;
+export default responsiveFontSizes(theme);
